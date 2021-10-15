75da69b007dfed5aa58aa8ee1ae9d6ff3c9e2f9b
Evgeniy <Rovikhevik@gmail.com>
2021-10-15 15:37:50 +0300

    Добавьте файлы проекта.

---
+++ VizitkaOnline/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VizitkaOnline.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Registration()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult UserCabinet()
+        {
+            return View();
+        }
+    }
+}
